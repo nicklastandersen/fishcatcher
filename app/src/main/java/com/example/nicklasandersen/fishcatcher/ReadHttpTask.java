9663d384e4c37dcddeebcefb568f5384e6c1cd39
nicklasandersen <nicklas.t.andersen@gmail.com>
2016-11-04 23:46:52 +0100

    New new - Jari din hund.

---
+++ app/src/main/java/com/example/nicklasandersen/fishcatcher/ReadHttpTask.java
@@ -0,0 +1,22 @@
+package com.example.nicklasandersen.fishcatcher;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
+
+public class ReadHttpTask extends AsyncTask<String, Void, CharSequence> {
+    @Override
+    protected CharSequence doInBackground(String... urls) {
+        String urlString = urls[0];
+        try {
+            CharSequence result = HttpHelper.GetHttpResponse(urlString);
+            return result;
+        } catch (IOException ex) {
+            cancel(true);
+            String errorMessage = ex.getMessage() + "\n" + urlString;
+            Log.e("BOOK", errorMessage);
+            return errorMessage;
+        }
+    }
+}
